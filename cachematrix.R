47f70a5548cce5e48437047169d887a2eb6894db
preity20 <preity.yadav2010@gmail.com>
2020-04-09 16:08:14 +0530

    documentation update for assignment

--- cachematrix.R
+++ cachematrix.R
@@ -4,16 +4,16 @@
 
 ## This function creates a special "matrix" object that can cache its inverse.
 
-makeCacheMatrix <- function(x = matrix()) {
-     matinv <- NULL
-     set <- function(y) {
-          x <<- y
-          matinv <<- NULL
+makeCacheMatrix <- function(x = matrix()) { ## define the argument with default mode of "matrix"
+     matinv <- NULL                ## initialize inv as NULL; will hold value of matrix inverse 
+     set <- function(y) {          ## define the set function to assign new 
+          x <<- y                  ## value of matrix in parent environment
+          matinv <<- NULL          ## if there is a new matrix, reset inv to NULL
      }
-     get <- function() x
-     setinv <- function(inverse) matinv <<- inverse
-     getinv <- function() matinv
-     list(set = set, get = get,
+     get <- function() x           ## define the get fucntion - returns value of the matrix argument
+     setinv <- function(inverse) matinv <<- inverse ## assigns value of inv in parent environment
+     getinv <- function() matinv   ## gets the value of inv where called
+     list(set = set, get = get,    ## returns list
           setinv = setinv,
           getinv = getinv)
 }
@@ -26,12 +26,12 @@ makeCacheMatrix <- function(x = matrix()) {
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
      matinv <- x$getinv()
-     if(!is.null(matinv)) {
-          message("getting cached data")
+     if(!is.null(matinv)) {             ##checks to see if inverse has already been calculated
+          message("getting cached data") ##If so, it gets the inverse from the cache and skips the computation
           return(matinv)
      }
      data <- x$get()
-     matinv <- solve(data, ...)
-     x$setinv(matinv)
-     matinv
+     matinv <- solve(data, ...)  ## calculates inverse of matrix
+     x$setinv(matinv)         ##sets the value of the inverse in the cache via the setinv function
+     matinv                   ## returns inverse of the matrix
 }
